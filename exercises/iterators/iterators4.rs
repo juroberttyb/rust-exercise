c5b8cb3b0595a306cab164e2669b6b36be9c1131
robert <juroberttyb@gmail.com>
2023-06-11 01:39:07 +0800

    progress: 78.9%

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,8 +1,6 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -14,7 +12,12 @@ pub fn factorial(num: u64) -> u64 {
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
 
-    num
+    match num {
+        0 => 1,
+        1.. => (1..=num).rev().collect::<Vec<u64>>().iter().fold(1, |acc, &x| acc * x)
+    }
+
+    // num
 }
 
 #[cfg(test)]
